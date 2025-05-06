9b08436e1109f3bb403c22683dce7e54210161ec
alexagold1 <526agold@frhsd.com>
2025-05-06 13:36:47 -0400

    removed the h1 pages that were in the book

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ document.querySelector(".cover").addEventListener("click", function () {
     }
   }, 500);
 });
-// JavaScript for iterator that updates when arrows are pressed
 
 const iteratorMessages = [
   "You're on Page 1",
@@ -25,19 +24,16 @@ const iteratorMessages = [
 
 const iteratorDisplay = document.getElementById("iterator-display");
 
-// Track the current page (starting from Page 1)
 let currentPage = 1;
 
-// Function to update iterator message based on the current page
 function updateIteratorMessage(page) {
   iteratorDisplay.textContent = iteratorMessages[page - 1];
 }
 
-// Event listeners for the 'next' and 'prev' buttons
 document.querySelectorAll(".next").forEach((button, index) => {
   button.addEventListener("click", function () {
     if (currentPage < 3) {
-      currentPage++; // Go to next page
+      currentPage++;
       updateIteratorMessage(currentPage);
     }
   });
@@ -46,7 +42,7 @@ document.querySelectorAll(".next").forEach((button, index) => {
 document.querySelectorAll(".prev").forEach((button, index) => {
   button.addEventListener("click", function () {
     if (currentPage > 1) {
-      currentPage--; // Go to previous page
+      currentPage--;
       updateIteratorMessage(currentPage);
     }
   });
